d3cb50812634d2cee318857419a104fb31561047
guilherme henrique <ghsmmvts@gmail.com>
2019-07-16 11:21:00 -0300

    estrutura do jogo java script

---
+++ script.js
@@ -0,0 +1,64 @@
+// let imagensSalvas = ["Coco.png", "crash.png", "FakeCrash.png", "Neo.png", "Oxide.png", "Penta.png", "Pura.png", "RipperRoo.png"];
+
+// let imagens = imagensSalvas.concat(imagensSalvas);
+
+// let cartas = document.querySelectorAll(".carta");
+
+// imagens = embaralhar (imagens);
+
+// for(let i in cartas){
+//     cartas[i].style.backgroundImage = `url("../Characters/${imagens[i]}")`
+// }
+
+// console.log
+
+// function embaralhar(lista) {
+   
+//     let valor_temporario;
+//     let indice_aleatorio;
+    
+//     for (let i = lista.length -1; i !== 0; i--){
+//         indice_aleatorio = Math.floor(Math.random() * i);
+
+//       valor_temporario = lista[i];
+//       lista[i] = lista[indice_aleatorio];
+//       lista[indice_aleatorio] = valor_temporario;
+//     }
+//     return lista;
+// }
+ 
+//  for(let i in cartas){
+//      cartas[i]
+//  }
+
+function embaralhar(lista) {
+    let valor_temporario;
+    let indice_aleatorio;
+  
+    for (let i = lista.length -1; i !== 0; i--) {
+        indice_aleatorio = Math.floor(Math.random() * i);
+  
+        valor_temporario = lista[i];
+        lista[i] = lista[indice_aleatorio];
+        lista[indice_aleatorio] = valor_temporario;
+    }
+    return lista;
+  }
+  
+  let cartas = document.querySelectorAll(".carta");
+  
+  let imagensSalvas = ["Coco.png", "crash.png", "FakeCrash.png", "Neo.png", "Oxide.png", "Penta.png", "Pura.png", "RipperRoo.png"];
+  
+  let imagens = imagensSalvas.concat(imagensSalvas);
+  
+  imagens = embaralhar(imagens);
+  
+  for(let i = 0; i < cartas.length; i++){
+    cartas[i].style.backgroundImage = `url("imagens/${imagens[i]}")`;
+  }
+  
+  setTimeout(function(){
+    for(let carta of cartas){
+      carta.style.backgroundImage = 'url("imagens/caixa.png")';
+    }
+  }, 3000);  
